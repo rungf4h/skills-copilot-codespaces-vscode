742e5be116affd53128d983c95cb9fa5a7801e3b
Rafael Hdez <157428247+rungf4h@users.noreply.github.com>
2024-06-15 11:08:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var memberSkills = {
+        name: 'Sally',
+        age: 25,
+        skills: ['JavaScript', 'React', 'Node'],
+        details: {
+            id: 1,
+            title: 'Programmer'
+        }
+    };
+    return memberSkills;
+}
